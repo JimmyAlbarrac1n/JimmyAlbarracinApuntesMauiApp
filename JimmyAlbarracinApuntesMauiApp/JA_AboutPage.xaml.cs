f939fb3a26fe64cce1c05b59739759307917c502
JimmyAlbarrac1n <jimmismael2003@gmail.com>
2024-05-29 11:59:23 -0500

    NotePage creada e implementada, iconos agregados

--- JimmyAlbarracinApuntesMauiApp/JA_AboutPage.xaml.cs
+++ JimmyAlbarracinApuntesMauiApp/JA_AboutPage.xaml.cs
@@ -7,7 +7,7 @@ public partial class JA_AboutPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void LearnMore_Clicked(object sender, EventArgs e)
+    private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         // Navigate to the specified URL in the system browser.
         await Launcher.Default.OpenAsync("https://aka.ms/maui");
